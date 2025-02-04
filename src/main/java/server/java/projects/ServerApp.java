f8e7b5a24aaea7c8e327fd8c386406561aa57517
Janinda Rukshan <janruk91@gmail.com>
2025-02-04 11:29:17 +0530

    Update main method to separate the HTTP request header

--- src/main/java/server/java/projects/ServerApp.java
+++ src/main/java/server/java/projects/ServerApp.java
@@ -1,8 +1,50 @@
 package server.java.projects;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(80);
+        System.out.println("Server started on port 80");
 
+        while (true) {
+            Socket localSocket = serverSocket.accept();
+            System.out.println("Accepted connection from " + localSocket.getRemoteSocketAddress());
+
+            new Thread(() -> {
+                try {
+                    InputStream is = localSocket.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(is);
+                    BufferedReader br = new BufferedReader(isr);
+
+                    String commandLine = br.readLine();
+                    String command = commandLine.split(" ")[0].strip();
+                    String resourcePath = commandLine.split(" ")[1].strip();
+                    System.out.println("Command: " + command);
+                    System.out.println("ResourcePath: " + resourcePath);
+
+                    String host = null;
+                    String line;
+                    while ((line = br.readLine()) != null && !line.isBlank()) {
+                        String header = line.split(":")[0].strip();
+                        String value = line.substring(line.indexOf(":") + 1);
+                        if (header.equalsIgnoreCase("host")) {
+                            host = value.strip();
+                        }
+                    }
+
+                    System.out.println("Host: " + host);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }).start();
+        }
     }
 }
