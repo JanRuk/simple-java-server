c297b29d29dd4697b508037aa81e79b3f7398aa9
Janinda Rukshan <janruk91@gmail.com>
2025-02-04 17:13:03 +0530

    Update main method to respond to 400 and 400 status codes

--- src/main/java/server/java/projects/ServerApp.java
+++ src/main/java/server/java/projects/ServerApp.java
@@ -1,11 +1,9 @@
 package server.java.projects;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDateTime;
 
 public class ServerApp {
 
@@ -38,8 +36,62 @@ public class ServerApp {
                             host = value.strip();
                         }
                     }
-
                     System.out.println("Host: " + host);
+
+                    OutputStream os = localSocket.getOutputStream();
+                    OutputStreamWriter osr = new OutputStreamWriter(os);
+                    BufferedWriter bw = new BufferedWriter(osr);
+
+                    if (!command.equals("GET")) {
+                        String httpHeader = """
+                                HTTP/1.1 405 Method Not Allowed
+                                Date: %s
+                                Server: Simple Java Server
+                                
+                                """.formatted(LocalDateTime.now());
+
+                        bw.write(httpHeader);
+                        bw.flush();
+
+                        String httpBody = """
+                                <!DOCTYPE html>
+                                <html lang="en">
+                                <head>
+                                    <meta charset="UTF-8">
+                                    <title>405</title>
+                                </head>
+                                <body>
+                                    <h1>405</h1>
+                                    <h2>HTTP Method Not Allowed</h2>
+                                </body>
+                                </html>
+                                """;
+                        bw.write(httpBody);
+                        bw.flush();
+                    } else if (host == null) {
+                        String httpHeader = """
+                                HTTP/1.1 400 Bad Request
+                                Date: %s
+                                Server: Simple Java Server
+                                """.formatted(LocalDateTime.now());
+                        bw.write(httpHeader);
+                        bw.flush();
+                        String httpBody = """
+                                <!DOCTYPE html>
+                            <html lang="en">
+                            <head>
+                                <meta charset="UTF-8">
+                                <title>Bad Request</title>
+                            </head>
+                            <body>
+                                <h1>400</h1>
+                                <h2>Bad Request</h2>
+                            </body>
+                            </html>
+                            """;
+                        bw.write(httpBody);
+                        bw.flush();
+                    }
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
